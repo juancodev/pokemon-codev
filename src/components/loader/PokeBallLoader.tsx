eb45212a40e4b519331913495d8aa91cf5dd8813
juancodev <montillasanchezjuancarlos@gmail.com>
2023-06-29 13:18:45 -0400

    [⚡ADD] Loader: PokeBallLoader

---
+++ src/components/loader/PokeBallLoader.tsx
@@ -0,0 +1,22 @@
+import "./PokeBallLoader.css";
+
+const PokeBallLoader = () => {
+  return (
+    <>
+      <div className="container-loaders">
+        <div className="loader"></div>
+        <div className="spinner">
+          <span>P</span>
+          <span>O</span>
+          <span>K</span>
+          <span>É</span>
+          <span>D</span>
+          <span>U</span>
+          <span>X</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export { PokeBallLoader };
---
+++ src/components/pokemoncard/PokeInfo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PokeInfo = () => {
+  return <div>PokeInfo</div>;
+};
+
+export default PokeInfo;
